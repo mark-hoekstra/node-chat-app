c4ef83d3e3c52e79dfaee7c596784d92297430c8
Mark Hoekstra <m.a.hoekstra@outlook.com>
2018-12-04 15:37:44 +0100

    added events

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-var app = express();
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(port, () => {
+io.on('connection', (socket)=>{
+    console.log('New user connected')
+
+    socket.on('disconnect', ()=>{
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started up on port ${port}`);
   });
-  
-module.exports = {app};
