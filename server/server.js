b140e95687d7b0fd7e2de9299f77ba0cd5eb0ff2
Mark Hoekstra <m.a.hoekstra@outlook.com>
2018-12-04 14:01:10 +0100

    first init

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+    console.log(`Started up on port ${port}`);
+  });
+  
+module.exports = {app};
